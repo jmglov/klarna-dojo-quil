d63955ed5800486f36a8317110b0e2c5e902d0a4
Joshua Glover <joshua.glover@klarna.com>
2016-11-28 14:55:57 +0100

    ClojureBridge: first program rainbow lines
    https://github.com/ClojureBridge/drawing/blob/master/curriculum/first-program.md#exercise-rainbow-lines

--- src/klarna_dojo_quil/core.cljs
+++ src/klarna_dojo_quil/core.cljs
@@ -4,19 +4,22 @@
 
 (defn setup []
   (q/frame-rate 30)
-  (q/color-mode :rgb)
-  (q/stroke 255 0 0))
+  (q/color-mode :rgb))
+
+(defn line [[r g b] [x y]]
+  (q/stroke r g b)
+  (q/line x y (q/mouse-x) (q/mouse-y)))
 
 (defn draw [state]
-  (q/line 0 0 (q/mouse-x) (q/mouse-y))
-  (q/line 200 0 (q/mouse-x) (q/mouse-y))
-  (q/line 0 200 (q/mouse-x) (q/mouse-y))
-  (q/line 200 200 (q/mouse-x) (q/mouse-y)))
+  (line [255 0 0] [0 0])
+  (line [255 255 0] [500 0])
+  (line [0 255 0] [0 500])
+  (line [0 0 255] [500 500]))
 
 (q/defsketch klarna-dojo-quil
-  :title "You can see lines"
+  :title "Yay! Rainbows!"
   :host "klarna-dojo-quil"
-  :size [500 500]
+  :size [510 510]
   :setup setup
   :draw draw
   :features [:keep-on-top])
