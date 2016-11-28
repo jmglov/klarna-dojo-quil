1ee6252482bce133b84b45d0c04888dcf7cbb99a
Joshua Glover <joshua.glover@klarna.com>
2016-11-28 16:30:14 +0100

    ClojureBridge: first program dynamic rainbow

--- src/klarna_dojo_quil/core.cljs
+++ src/klarna_dojo_quil/core.cljs
@@ -2,24 +2,41 @@
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]))
 
+(def width 510)
+(def height 510)
+
 (defn setup []
   (q/frame-rate 30)
   (q/color-mode :rgb))
 
-(defn line [[r g b] [x y]]
-  (q/stroke r g b)
-  (q/line x y (q/mouse-x) (q/mouse-y)))
+(defn- ->color-component [c]
+  (if (sequential? c)
+    (let [pct-x (/ (q/mouse-x) width)
+          pct-y (/ (q/mouse-y) height)
+          pct (/ (+ pct-x pct-y) 2)
+          [start end] c
+          range-size (Math/abs (- start end))
+          delta (* range-size pct)]
+      (if (< start end)
+        (+ start delta)
+        (+ end delta)))
+    c))
+
+(defn- line [color [x y]]
+  (let [[r g b] (map ->color-component color)]
+    (q/stroke r g b)
+    (q/line x y (q/mouse-x) (q/mouse-y))))
 
 (defn draw [state]
-  (line [255 0 0] [0 0])
-  (line [255 255 0] [500 0])
-  (line [0 255 0] [0 500])
-  (line [0 0 255] [500 500]))
+  (line [255 [0 127] 0] [0 0])
+  (line [[255 0] 255 0] [500 0])
+  (line [0 [255 0] [0 255]] [0 500])
+  (line [[0 143] 0 255] [500 500]))
 
 (q/defsketch klarna-dojo-quil
   :title "Yay! Rainbows!"
   :host "klarna-dojo-quil"
-  :size [510 510]
+  :size [width height]
   :setup setup
   :draw draw
   :features [:keep-on-top])
